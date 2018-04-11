e08a2f0988d31a864658dfe0cab5c9593e819bb5
yanruifeng <xlyanrui>
2018-04-11 14:30:56 +0800

    MainActivity

--- app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
@@ -15,29 +15,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.tl_tab)
-    TabLayout tlTab;
-    @BindView(R.id.vp_pager)
-    ViewPager vpPager;
-    private String[] tabTitleArray = {"要闻", "英雄联盟", "守望先锋"};
-    private List<Fragment> list;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//        //此处使用for循环
-//        for (int i = 0; i < tabTitleArray.length; i++) {
-//            tlTab.addTab(tlTab.newTab().setText(tabTitleArray[i]));
-//        }
-//        tlTab.setTabMode(TabLayout.MODE_FIXED);
-//        list = new ArrayList<>();
-//        list.add(new NewsFragment());
-//        list.add(new SwxfFragment());
-//        list.add(new YxlmFragment());
-//        vpPager.setAdapter(new MyAdapter(getSupportFragmentManager(), list, tabTitleArray));
-//        tlTab.setupWithViewPager(vpPager);
     }
 
     public void onResume() {
