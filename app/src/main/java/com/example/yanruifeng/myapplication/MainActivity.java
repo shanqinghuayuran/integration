c744c320780afda5903e781ed5b78c5aebde910e
yanruifeng <xlyanrui>
2018-04-11 17:42:13 +0800

    新增index.html

--- app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
@@ -1,34 +1,48 @@
 package com.example.yanruifeng.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import com.umeng.analytics.AnalyticsConfig;
-import com.umeng.analytics.MobclickAgent;
+import android.webkit.WebSettings;
+import android.widget.Button;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.bt_Test)
+    Button btTest;
+    @BindView(R.id.bw_webview)
+    BridgeWebView bwWebview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        bwWebview.loadUrl("file:///android_asset/index.html");
+        bwWebview.registerHandler("Android", new BridgeHandler() {
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                Toast.makeText(MainActivity.this, "H5给我的数据：" + data, Toast.LENGTH_SHORT).show();
+                function.onCallBack("fuck!");
+            }
+        });
+    }
+    @OnClick({R.id.bt_Test})
+    public void click(){
+        //给js发送消息
+        bwWebview.callHandler("functionInJs", "传递消息给h5", new CallBackFunction() {
+            @Override
+            public void onCallBack(String data) {
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void onResume() {
-        super.onResume();
-        MobclickAgent.onResume(this);
-    }
-
-    public void onPause() {
-        super.onPause();
-        MobclickAgent.onPause(this);
-    }
 }
