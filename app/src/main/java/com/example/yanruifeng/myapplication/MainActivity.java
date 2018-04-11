fab7832c25edb9d258789bcd498072704e8d1466
yanruifeng <xlyanrui>
2018-04-11 21:39:01 +0800

    更新MainActivity

--- app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.yanruifeng.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebSettings;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.yanruifeng.myapplication.bean.MessageEvent;
 import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
 import com.github.lzyzsd.jsbridge.CallBackFunction;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,11 +26,15 @@ public class MainActivity extends AppCompatActivity {
     Button btTest;
     @BindView(R.id.bw_webview)
     BridgeWebView bwWebview;
+    @BindView(R.id.bt_testEventBus)
+    Button btTestEventBus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //注册EventBus
+        EventBus.getDefault().register(this);
         ButterKnife.bind(this);
         bwWebview.loadUrl("file:///android_asset/index.html");
         bwWebview.registerHandler("Android", new BridgeHandler() {
@@ -34,15 +45,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    @OnClick({R.id.bt_Test})
-    public void click(){
-        //给js发送消息
-        bwWebview.callHandler("functionInJs", "传递消息给h5", new CallBackFunction() {
-            @Override
-            public void onCallBack(String data) {
-                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
-            }
-        });
+
+    @OnClick({R.id.bt_Test,R.id.bt_testEventBus})
+    public void click(View v) {
+        switch (v.getId()) {
+            //给js发送消息
+            case R.id.bt_Test:
+                bwWebview.callHandler("functionInJs", "传递消息给h5", new CallBackFunction() {
+                @Override
+                public void onCallBack(String data) {
+                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                }
+            });
+                break;
+            case R.id.bt_testEventBus:
+                    Intent intent=new Intent(this,SecondActivity.class);
+                    startActivity(intent);
+                break;
+        }
+    }
+    //订阅 获取信息
+    @Subscribe(threadMode = ThreadMode.POSTING)
+    public void onEvent(MessageEvent event){
+        Log.d("tag",event.getMessage());
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //取消注册
+        EventBus.getDefault().unregister(this);
+    }
 }
