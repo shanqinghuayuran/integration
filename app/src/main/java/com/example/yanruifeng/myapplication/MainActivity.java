5874dc9180b55888c78f0966d98a856bbe4f1e7a
yanruifeng <xlyanrui>
2018-04-12 12:42:09 +0800

    修改主界面

--- app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yanruifeng/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.yanruifeng.myapplication.bean.MessageEvent;
+import com.example.yanruifeng.myapplication.utils.LogUtils;
 import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
 import com.github.lzyzsd.jsbridge.CallBackFunction;
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     BridgeWebView bwWebview;
     @BindView(R.id.bt_testEventBus)
     Button btTestEventBus;
+    @BindView(R.id.bt_testRetroftRxjava)
+    Button btTestRetroftRxjava;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         //注册EventBus
         EventBus.getDefault().register(this);
         ButterKnife.bind(this);
+       // LogUtils.d("1111",MyApp.getInstance().getApplicationContext());
         bwWebview.loadUrl("file:///android_asset/index.html");
         bwWebview.registerHandler("Android", new BridgeHandler() {
             @Override
@@ -46,28 +50,36 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @OnClick({R.id.bt_Test,R.id.bt_testEventBus})
+    @OnClick({R.id.bt_Test, R.id.bt_testEventBus,R.id.bt_testRetroftRxjava})
     public void click(View v) {
         switch (v.getId()) {
             //给js发送消息
             case R.id.bt_Test:
                 bwWebview.callHandler("functionInJs", "传递消息给h5", new CallBackFunction() {
-                @Override
-                public void onCallBack(String data) {
-                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
-                }
-            });
+                    @Override
+                    public void onCallBack(String data) {
+                        Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                    }
+                });
                 break;
             case R.id.bt_testEventBus:
-                    Intent intent=new Intent(this,SecondActivity.class);
-                    startActivity(intent);
+                Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.bt_testRetroftRxjava:
+                Intent intent1 = new Intent(this,  LoginActivity.class);
+                startActivity(intent1);
+                break;
+            default:
                 break;
         }
+
     }
+
     //订阅 获取信息
     @Subscribe(threadMode = ThreadMode.POSTING)
-    public void onEvent(MessageEvent event){
-        Log.d("tag",event.getMessage());
+    public void onEvent(MessageEvent event) {
+        Toast.makeText(this, event.getMessage(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
