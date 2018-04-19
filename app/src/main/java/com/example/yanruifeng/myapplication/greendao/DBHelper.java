188dc7adeb30e70aa72b3728b43bddb6a2af0fec
yanruifeng <xlyanrui>
2018-04-19 20:52:39 +0800

    更新

---
+++ app/src/main/java/com/example/yanruifeng/myapplication/greendao/DBHelper.java
@@ -0,0 +1,37 @@
+package com.example.yanruifeng.myapplication.greendao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.github.yuweiguocn.library.greendao.MigrationHelper;
+
+import org.greenrobot.greendao.database.Database;
+
+/**
+ * Created by yanruifeng on 2018/4/19.
+ * 数据库助手
+ */
+
+public class DBHelper extends DaoMaster.OpenHelper {
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+        super(context, name, factory);
+    }
+
+    @Override
+    public void onUpgrade(Database db, int oldVersion, int newVersion) {
+
+        //把需要管理的数据库表DAO作为最后一个参数传入到方法中
+        MigrationHelper.migrate(db, new MigrationHelper.ReCreateAllTableListener() {
+
+            @Override
+            public void onCreateAllTables(Database db, boolean ifNotExists) {
+                DaoMaster.createAllTables(db, ifNotExists);
+            }
+
+            @Override
+            public void onDropAllTables(Database db, boolean ifExists) {
+                DaoMaster.dropAllTables(db, ifExists);
+            }
+        },  UserDao.class);
+    }
+}
